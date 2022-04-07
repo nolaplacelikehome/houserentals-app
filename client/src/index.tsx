411912813ecb7b0c01c1268869fe0d49eb5d23fe
jakedwng <jakedwng@gmail.com>
2022-04-07 11:00:34 -0400

    Migrate to React v18 (which only changes the root API and renderer)

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import '../dist/css/App.css';
@@ -14,4 +14,6 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const container = document.getElementById('app')!;
+const root = createRoot(container);
+root.render(<App />);
